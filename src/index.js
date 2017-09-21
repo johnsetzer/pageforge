0ce815d7a843506dd8e543cc92ad52988cb517be
John Setzer <john.setzer@gmail.com>
2017-09-21 12:22:42 -0700

    Added support for Redux devtools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import reducers from './redux/reducers'
 import App from './App'
 import './index.css'
 
-let store = createStore(reducers)
+let store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
