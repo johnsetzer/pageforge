6f312d8d3229b4e0c1ab0dabcd6e3c90b1680158
John Setzer <john.setzer@gmail.com>
2017-09-21 00:12:56 -0700

    Fixed App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './App'
 import reducers from './redux/reducers'
@@ -8,5 +9,10 @@ import reducers from './redux/reducers'
 it('renders without crashing', () => {
   const store = createStore(reducers)
   const div = document.createElement('div')
-  ReactDOM.render(<App store={store}/>, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
 })
