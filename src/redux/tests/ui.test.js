789b3637a4f9ab6831893e8629bffa6ebc8de4e0
John Setzer <john.setzer@gmail.com>
2017-09-21 00:17:11 -0700

    Refactored ui.test.js

--- src/redux/tests/ui.test.js
+++ src/redux/tests/ui.test.js
@@ -3,10 +3,14 @@ import ui from '../reducers/ui'
 import Actions from '../action_creators'
 import deepFreeze from 'deep-freeze'
 
-it('has initial state', () => {
-  let store = createStore(ui)
-  deepFreeze(store.getState())
+let store
 
+beforeEach(() => {
+  store = createStore(ui)
+  deepFreeze(store.getState())
+})
+
+it('sets initial state', () => {
   expect(store.getState()).toEqual({
     designerModalOpen: false,
     helpModalOpen: false,
@@ -15,19 +19,14 @@ it('has initial state', () => {
 })
 
 it('sets mode', () => {
-  let store = createStore(ui)
-  deepFreeze(store.getState())
-
   store.dispatch(Actions.setMode('ADD_COMPONENT'))
 
   expect(store.getState().mode).toBe('ADD_COMPONENT')
 })
 
 it('toggles modal flags', () => {
-  let store = createStore(ui)
-  deepFreeze(store.getState())
-
   store.dispatch(Actions.toggleModal('designer'))
-
   expect(store.getState().designerModalOpen).toBe(true)
+  store.dispatch(Actions.toggleModal('designer'))
+  expect(store.getState().designerModalOpen).toBe(false)
 })
