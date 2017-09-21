f3b34e28b3d9a0fb2cbfa1e5ec56bc74809697ac
John Setzer <john.setzer@gmail.com>
2017-09-21 08:17:07 -0700

    Changed action creators to return object expressions

--- src/redux/action_creators/components.js
+++ src/redux/action_creators/components.js
@@ -5,48 +5,38 @@ const resetComponentCreator = () => {
   nextId = 1
 }
 
-const createComponent = (left, top, width, height) => {
-  return {
-    type: 'CREATE_COMPONENT',
-    id: nextId++,
-    left,
-    top,
-    width,
-    height
-  }
-}
+const createComponent = (left, top, width, height) => ({
+  type: 'CREATE_COMPONENT',
+  id: nextId++,
+  left,
+  top,
+  width,
+  height
+})
 
-const selectComponent = id => {
-  return {
-    type: 'SELECT_COMPONENT',
-    id
-  }
-}
+const selectComponent = id => ({
+  type: 'SELECT_COMPONENT',
+  id
+})
 
-const moveComponent = (id, left, top) => {
-  return {
-    type: 'MOVE_COMPONENT',
-    id,
-    left,
-    top
-  }
-}
+const moveComponent = (id, left, top) => ({
+  type: 'MOVE_COMPONENT',
+  id,
+  left,
+  top
+})
 
-const renameComponent = (id, name) => {
-  return {
-    type: 'RENAME_COMPONENT',
-    id,
-    name
-  }
-}
+const renameComponent = (id, name) => ({
+  type: 'RENAME_COMPONENT',
+  id,
+  name
+})
 
-const setComponentStyles = (id, styles) => {
-  return {
-    type: 'SET_COMPONENT_STYLES',
-    id,
-    styles
-  }
-}
+const setComponentStyles = (id, styles) => ({
+  type: 'SET_COMPONENT_STYLES',
+  id,
+  styles
+})
 
 export default {
   resetComponentCreator,
